5a3c722b4887888ea42d4d4021680c843a215c7b
Kirill Rudenko <cs1@list.ru>
2022-09-22 23:51:01 +0300

    allow the user to specify a different filepath

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import os
+import argparse
 import collections
 import datetime
+
 from pathlib import Path
-
-import pandas
-
+from argparse import RawTextHelpFormatter
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 WINERY_YEAR_FOUNDED = 1920
-WINE_FILENAME = 'wine.xlsx'
 
 
 def get_age_suffix(age: int) ->  str:
@@ -35,21 +36,36 @@ def load_wine_categories(filepath: Path) -> dict:
     return wines
 
 
-def main():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+def create_argparser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="""\
+        Скрипт запускает на локальной машине сайт-магазин по продаже вина.\n
+        Для запуска требуется указать файл с перечнем продаваемой продукции.
+        По умолчанию, скрипт пробует открыть файл "wine.xlsx", находящийся в\n
+        директории с запускаемым скриптом, если не указан иной путь до файла.""",
+        formatter_class=RawTextHelpFormatter
     )
+    parser.add_argument('-fp', '--filepath', help='Путь до файла с данными', type=str)
+    return parser.parse_args()
 
-    template = env.get_template('template.html')
+
+def main():
+    args = create_argparser()
+    filepath = os.getenv("FILEPATH", default='wine.xlsx')
+    if args.filepath: filepath = args.filepath
+        
+    wine_filepath = Path(filepath)
+    wine_categories = load_wine_categories(wine_filepath)
 
     today = datetime.date.today()
     winery_age = today.year - WINERY_YEAR_FOUNDED
     winery_age_suffix = get_age_suffix(winery_age)
-
-    wine_filepath = Path(WINE_FILENAME)
-    wine_categories = load_wine_categories(wine_filepath)
-            
+    
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
     rendered_page = template.render(
         wine_categories = wine_categories
     )
